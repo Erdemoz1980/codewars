8c25f758c18d0c26051c95dec080bca0933b60dc
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-12-02 23:56:39 -0500

    regex intro

--- regular-expressions/script.js
+++ regular-expressions/script.js
@@ -2,16 +2,18 @@ function validatePIN (pin) {
   const splitInput = pin.split('');
   const numbersOnly =/^\d+$/;
   
-if(!numbersOnly.test(pin)) return false;
+if(!numbersOnly.test(pin)) return console.log(false);
   
  if(splitInput.length === 4) {
-  return true
+  return console.log(true)
   }else if(splitInput.length ===6){
-  return true
+  return console.log(tre)
 } else{
-  return false
+  return console.log(false)
 }
 };
+
+validatePIN('1e45')
 
 //ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 
