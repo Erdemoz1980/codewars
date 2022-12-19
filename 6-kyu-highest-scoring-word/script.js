00700e21377f6e307fb0eef51b452db2ec44c7d1
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-12-18 21:47:56 -0500

    highest scoring word

---
+++ 6-kyu-highest-scoring-word/script.js
@@ -0,0 +1,27 @@
+function high(string) {
+  
+  const values = {
+    a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+  }
+  
+  let wordValues = {}
+  string.split(' ').forEach(word => {
+    for (i = 0; i < word.length; i++) {
+      wordValues[word] = word.split('').reduce((acc, char) => acc + values[char], 0);
+    };
+  })
+  
+  return Object.entries(wordValues).sort((a, b) => b[1] - a[1])[0][0]
+ 
+}
+
+/*Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
