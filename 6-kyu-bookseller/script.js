b69ba535c4d0c8c1fc3fc378928770618c27b81e
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-12-22 00:52:57 -0500

    worst challenge

--- 6-kyu-bookseller/script.js
+++ 6-kyu-bookseller/script.js
@@ -19,10 +19,19 @@ function stockList(books, cats){
   let arr = Object.entries(booksQts)
      
   for(i=0;i<arr.length;i++){
-     arr[i].splice(0,2,'(',arr[i][0],':',arr[i][1],')')           
+     arr[i].splice(0,2,'(',arr[i][0],' ',':',' ',arr[i][1],')',' ','-',' ')           
   }
   
-  console.log(arr)
+  const str = arr.flat().join('')
+  
+   const sliced = str.slice(0,str.length-2)
+   
+   console.log(sliced)
+  console.log("(A : 0) - (B : 1290) - (C : 515) - (D : 600)")
+   
+   return sliced
+  
+
   
   }
 
