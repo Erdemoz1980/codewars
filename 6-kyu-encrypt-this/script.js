c6026c0459f0808845eae93378933ca01404fe3b
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-12-30 19:04:24 -0500

    encryption found it really hard!

---
+++ 6-kyu-encrypt-this/script.js
@@ -0,0 +1,33 @@
+var encryptThis = function(text) {
+  if(text.length===1) return text.charCodeAt(0).toString()
+  
+  text = "A in oak"
+  const words = text.split(' ')
+
+  
+  for(i=0;i<words.length;i++){
+    if(words[i].length===1){
+       words.splice(i,1,words[i].charCodeAt(0));
+      
+    }else if (words[i].length===2){
+     let ascii = words[i].split('')[0].charCodeAt(0)
+     let split = words[i].split('');
+     split.splice(0,1,ascii)
+     let joined = split.join('')
+     words.splice(i,1,joined) 
+    
+  }else{
+    let ascii = words[i].split('')[0].charCodeAt(0);
+    let split = words[i].split('');
+    split.splice(0,1,ascii);
+ 
+    let second = split[1];
+    let last = split[split.length-1]
+    split.splice(1,1,last)
+    split.splice(split.length-1,1,second);
+  }
+  
+  }
+  
+  return words
+  }
