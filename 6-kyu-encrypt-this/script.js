0785f9988b0ebd70ec0f09e82ea74454bba7de7c
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-12-30 19:13:09 -0500

    encryption one of the trickiest finally!

--- 6-kyu-encrypt-this/script.js
+++ 6-kyu-encrypt-this/script.js
@@ -1,10 +1,8 @@
 var encryptThis = function(text) {
-  if(text.length===1) return text.charCodeAt(0).toString()
+  if(text.length===1) return text.charCodeAt(0).toString()  
   
-  text = "A in oak"
   const words = text.split(' ')
 
-  
   for(i=0;i<words.length;i++){
     if(words[i].length===1){
        words.splice(i,1,words[i].charCodeAt(0));
@@ -14,20 +12,24 @@ var encryptThis = function(text) {
      let split = words[i].split('');
      split.splice(0,1,ascii)
      let joined = split.join('')
-     words.splice(i,1,joined) 
+     words.splice(i,1,joined);
+      
     
   }else{
     let ascii = words[i].split('')[0].charCodeAt(0);
     let split = words[i].split('');
     split.splice(0,1,ascii);
- 
+  
     let second = split[1];
     let last = split[split.length-1]
     split.splice(1,1,last)
     split.splice(split.length-1,1,second);
+    let joined = split.join('')
+    words.splice(i,1,joined)
+    
   }
   
   }
   
-  return words
+  return words.join(' ')
   }
